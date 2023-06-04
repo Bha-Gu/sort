2ee64f053cb013d722751b835c515c96e7b2e2b4
Bha-Gu <bhaumik050402@gmail.com>
2023-06-04 11:12:25 +0530

    new file:   .gitignore 	new file:   Cargo.lock 	new file:   Cargo.toml 	new file:   src/main.rs

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+fn bubble<T>(arr: &mut Vec<T>) 
+where T: PartialOrd,
+{
+    for i in 0..arr.len() {
+        for j in 0..(arr.len() - 1 - i) {
+            if arr[j] > arr[j + 1] {
+                arr.swap(j, j+1);
+            }
+        }
+    }
+}
+
+fn main() {
+    let mut arr = vec![1,6586,145,463,14, 574,169,461,69875,4563,4,58,46358,43,5];
+    let mut arr2 = arr.clone();
+    let arrr = arr.clone();
+    arr.sort();
+    bubble( &mut arr2 );
+    println!("{:?}\n{:?}\n{:?}", arrr, arr, arr2);
+}
