72d0e4510d9624071e363d111ab1f1b01d8a2c21
Bha-Gu <bhaumik050402@gmail.com>
2023-06-20 14:19:53 +0530

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 fn bubble<T>(arr: &mut Vec<T>) 
 where T: PartialOrd,
 {
-    for i in 0..arr.len() {
+    for i in 0..arr.len() - 1 {
+        let mut swapped = false;
         for j in 0..(arr.len() - 1 - i) {
             if arr[j] > arr[j + 1] {
                 arr.swap(j, j+1);
+                swapped = true;
             }
         }
+        if !swapped{
+            break;
+        }
+        
     }
 }
 
